99e608159ab38d5f6bde455e4977a95ee2db6547
hayato_52b <a99246780@icloud.com>
2024-12-11 14:26:36 +0900

    CORSをrailsに設定

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,16 +1,17 @@
-# Be sure to restart your server when you modify this file.
+# このファイルを変更した場合は、必ずサーバーを再起動してください。
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+# フロントエンドアプリからAPIが呼び出される際のCORSの問題を回避します。
+
+# クロスオリジンのAjaxリクエストを受け入れるために、クロスオリジンリソース共有（CORS）を処理します。
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "localhost:5173"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
